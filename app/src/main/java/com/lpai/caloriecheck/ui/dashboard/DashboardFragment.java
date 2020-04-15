587576a8f44f09292e88006a355a52226ec48ac0
Stanciu Florin-Alexandru <stanciu.florin38@gmail.com>
2020-04-16 00:47:06 +0300

    custom list item

--- app/src/main/java/com/lpai/caloriecheck/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/lpai/caloriecheck/ui/dashboard/DashboardFragment.java
@@ -1,23 +1,28 @@
 package com.lpai.caloriecheck.ui.dashboard;
 
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-
 import androidx.fragment.app.ListFragment;
-
 import androidx.lifecycle.ViewModelProvider;
-
 import com.lpai.caloriecheck.R;
 
+import java.util.ArrayList;
+
+
 public class DashboardFragment extends ListFragment implements AdapterView.OnItemClickListener{
+
+    ArrayList<FoodItemModel> data;
+    ListView listView;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         DashboardViewModel dashboardViewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
@@ -30,14 +35,30 @@ public class DashboardFragment extends ListFragment implements AdapterView.OnIte
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.Company, android.R.layout.simple_list_item_1);
+
+        data = new ArrayList<>(); data.add(new FoodItemModel("Apple Pie", "Android 1.0", "1", "September 23, 2008"));
+        data.add(new FoodItemModel("Banana Bread", "Android 1.1", "2", "February 9, 2009"));
+        data.add(new FoodItemModel("Cupcake", "Android 1.5", "3", "April 27, 2009"));
+        data.add(new FoodItemModel("Donut", "Android 1.6", "4", "September 15, 2009"));
+        data.add(new FoodItemModel("Eclair", "Android 2.0", "5", "October 26, 2009"));
+        data.add(new FoodItemModel("Froyo", "Android 2.2", "8", "May 20, 2010"));
+        data.add(new FoodItemModel("Gingerbread", "Android 2.3", "9", "December 6, 2010"));
+        data.add(new FoodItemModel("Honeycomb", "Android 3.0", "11", "February 22, 2011"));
+        data.add(new FoodItemModel("Ice Cream Sandwich", "Android 4.0", "14", "October 18, 2011"));
+        data.add(new FoodItemModel("Jelly Bean", "Android 4.2", "16", "July 9, 2012"));
+        data.add(new FoodItemModel("Kitkat", "Android 4.4", "19", "October 31, 2013"));
+        data.add(new FoodItemModel("Lollipop", "Android 5.0", "21", "November 12, 2014"));
+        data.add(new FoodItemModel("Marshmallow", "Android 6.0", "23", "October 5, 2015"));
+
+        CustomAdapter adapter = new CustomAdapter(data, getActivity());
         setListAdapter(adapter);
         getListView().setOnItemClickListener(this);
+
     }
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
-        String[] mess = getResources().getStringArray(R.array.Company);
-        Toast.makeText(getActivity(), "Item: " + mess[position], Toast.LENGTH_SHORT).show();
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String mess=data.get(position).name;
+        Toast.makeText(getActivity(), "Item: " + mess, Toast.LENGTH_SHORT).show();
     }
 
 }
