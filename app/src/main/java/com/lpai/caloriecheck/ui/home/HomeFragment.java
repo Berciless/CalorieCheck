e0c38fe4add866ad3b5a2c86e346369597049f55
panaghiageorge <38526106+panaghiageorge@users.noreply.github.com>
2020-05-03 14:00:52 +0300

    formular bmi

--- app/src/main/java/com/lpai/caloriecheck/ui/home/HomeFragment.java
+++ app/src/main/java/com/lpai/caloriecheck/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,9 +18,32 @@ import com.lpai.caloriecheck.R;
 
 public class HomeFragment extends Fragment {
 
+    ProgressBar progress;
+    ProgressBar progressCarbs;
+    ProgressBar progressFat;
+    ProgressBar progressProtein;
+
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        progress = (ProgressBar) root.findViewById(R.id.progressBar);
+        progress.setProgress(800);
+        progress.setMax(1000);
+
+        progressCarbs = (ProgressBar) root.findViewById(R.id.progressBar_carbs);
+        progressCarbs.setProgress(400);
+        progressCarbs.setMax(1000);
+
+        progressFat = (ProgressBar) root.findViewById(R.id.progressBar_fats);
+        progressFat.setProgress(250);
+        progressFat.setMax(1000);
+
+        progressProtein = (ProgressBar) root.findViewById(R.id.progressBar_protein);
+        progressProtein.setProgress(90);
+        progressProtein.setMax(1000);
+
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), s -> textView.setText(s));
         return root;
--- app/src/main/java/com/lpai/caloriecheck/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/lpai/caloriecheck/ui/notifications/NotificationsFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,12 +21,61 @@ import com.lpai.caloriecheck.R;
 
 public class NotificationsFragment extends Fragment {
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    Spinner spinner;
+    Spinner spinnerSex;
+    EditText age;
+    EditText weight;
+    EditText height;
+    TextView results;
+    Button btnResults;
+    View root;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         NotificationsViewModel notificationsViewModel = new ViewModelProvider(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        root = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        spinner = (Spinner) root.findViewById(R.id.activitySpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
+                R.array.activity_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinnerSex = (Spinner) root.findViewById(R.id.sex_spinner);
+        ArrayAdapter<CharSequence> adapterSex = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
+                R.array.sex_array, android.R.layout.simple_spinner_item);
+        adapterSex.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSex.setAdapter(adapterSex);
+
+        weight = (EditText) root.findViewById(R.id.weight);
+        height = (EditText) root.findViewById(R.id.height);
+        age = (EditText) root.findViewById(R.id.age);
+        btnResults = (Button) root.findViewById(R.id.buttonResults);
+        results = (TextView) root.findViewById(R.id.textResult);
+        btnResults.setOnClickListener(v -> {
+            double W = Double.parseDouble(weight.getText().toString());
+            double H = Double.parseDouble(height.getText().toString());
+            double A = Double.parseDouble(age.getText().toString());
+            results.setText(String.valueOf(BMR(W,H,A)));
+        });
+
+
+
         final TextView textView = root.findViewById(R.id.text_notifications);
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), s -> textView.setText(s));
         return root;
     }
+
+    public double BMR(double W, double H, double A) {
+        String text = spinnerSex.getSelectedItem().toString();
+        System.out.println(text);
+
+        if (text.equals("Male")) {
+            return 10*W + 6.25*H - 5*A +5;
+        } else {
+            return  10*W + 6.25*H - 5*A -161;
+        }
+    }
+
 }
